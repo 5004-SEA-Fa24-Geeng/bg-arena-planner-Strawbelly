bd0015bd87771f2814a05ce116a6c9d59fa8822b
hewen <dreamershim@163.com>
2025-02-26 21:55:20 -0800

    hw

--- src/main/java/student/GamesLoader.java
+++ src/main/java/student/GamesLoader.java
@@ -4,11 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -36,7 +32,7 @@ public final class GamesLoader {
      */
     public static Set<BoardGame> loadGamesFile(String filename) {
 
-        Set<BoardGame> games = new HashSet<>();
+        Set<BoardGame> games = new LinkedHashSet<>();
 
         List<String> lines;
         try {
