eefc72e219f0328c94120751aa2e02093644b886
hewen <dreamershim@163.com>
2025-03-07 10:43:07 -0800

    hw

--- src/main/java/student/Planner.java
+++ src/main/java/student/Planner.java
@@ -32,14 +32,14 @@ public class Planner implements IPlanner {
     public Stream<BoardGame> filter(String filter) {
 
         if (filter.equals("")) {
-            Stream<BoardGame> filteredGames = sortOn(currentGames.stream(), GameData.NAME, true);
+            Stream<BoardGame> filteredGames = Sorting.sortOn(currentGames.stream(), GameData.NAME, true);
             currentGames = filteredGames.collect(Collectors.toCollection(LinkedHashSet::new));
             return currentGames.stream();
         }
 
         if (!filter.contains(",")) {
             try {
-                Stream<BoardGame> filteredGames = sortOn(filterSingle(filter, currentGames.stream()),
+                Stream<BoardGame> filteredGames = Sorting.sortOn(filterSingle(filter, currentGames.stream()),
                         GameData.NAME, true);
                 currentGames = filteredGames.collect(Collectors.toCollection(LinkedHashSet::new));
                 return currentGames.stream();
@@ -60,7 +60,7 @@ public class Planner implements IPlanner {
                 break;
             }
         }
-        filteredStream = sortOn(currentGames.stream(), GameData.NAME, true);
+        filteredStream = Sorting.sortOn(currentGames.stream(), GameData.NAME, true);
         currentGames = filteredStream.collect(Collectors.toCollection(LinkedHashSet::new));
         return currentGames.stream();
     }
@@ -75,14 +75,14 @@ public class Planner implements IPlanner {
     @Override
     public Stream<BoardGame> filter(String filter, GameData sortOn) {
         if (filter.equals("")) {
-            Stream<BoardGame> filteredGames = sortOn(currentGames.stream(), sortOn, true);
+            Stream<BoardGame> filteredGames = Sorting.sortOn(currentGames.stream(), sortOn, true);
             currentGames = filteredGames.collect(Collectors.toCollection(LinkedHashSet::new));
             return currentGames.stream();
         }
 
         if (!filter.contains(",")) {
             try {
-                Stream<BoardGame> filteredGames = sortOn(filterSingle(filter, currentGames.stream()), sortOn, true);
+                Stream<BoardGame> filteredGames = Sorting.sortOn(filterSingle(filter, currentGames.stream()), sortOn, true);
                 currentGames = filteredGames.collect(Collectors.toCollection(LinkedHashSet::new));
                 return currentGames.stream();
             } catch (IllegalArgumentException e) {
@@ -101,7 +101,7 @@ public class Planner implements IPlanner {
                 break;
             }
         }
-        filteredStream = sortOn(currentGames.stream(), sortOn, true);
+        filteredStream = Sorting.sortOn(currentGames.stream(), sortOn, true);
         currentGames = filteredStream.collect(Collectors.toCollection(LinkedHashSet::new));
         return currentGames.stream();
     }
@@ -117,7 +117,7 @@ public class Planner implements IPlanner {
     @Override
     public Stream<BoardGame> filter(String filter, GameData sortOn, boolean ascending) {
         if (filter.equals("")) {
-            Stream<BoardGame> filteredGames = sortOn(currentGames.stream(), sortOn, ascending);
+            Stream<BoardGame> filteredGames = Sorting.sortOn(currentGames.stream(), sortOn, ascending);
             currentGames = filteredGames
                     .collect(Collectors.toCollection(LinkedHashSet::new));
             return currentGames.stream();
@@ -125,7 +125,7 @@ public class Planner implements IPlanner {
 
         if (!filter.contains(",")) {
             try {
-                Stream<BoardGame> filteredGames = sortOn(filterSingle(filter, currentGames.stream()),
+                Stream<BoardGame> filteredGames = Sorting.sortOn(filterSingle(filter, currentGames.stream()),
                         sortOn, ascending);
                 currentGames = filteredGames
                         .collect(Collectors.toCollection(LinkedHashSet::new));
@@ -147,7 +147,7 @@ public class Planner implements IPlanner {
                 break;
             }
         }
-        filteredStream = sortOn(currentGames.stream(), sortOn, ascending);
+        filteredStream = Sorting.sortOn(currentGames.stream(), sortOn, ascending);
         currentGames = filteredStream.collect(Collectors.toCollection(LinkedHashSet::new));
         return currentGames.stream();
     }
@@ -188,74 +188,6 @@ public class Planner implements IPlanner {
             throw new IllegalArgumentException("Contains non-numeric values.");
         }
         return filterGames.filter(game -> Filters.filter(game, column, operator, value));
-    }
-
-    /**
-     * Sorts the stream of board games based on the specified column and order.
-     * @param filterGames the stream of board games to be sorted.
-     * @param sortOn the column to sort the results on.
-     * @param ascending whether the sort is in ascending order.
-     * @return a sorted stream of board games based on the specified column and order.
-     */
-    private static Stream<BoardGame> sortOn(Stream<BoardGame> filterGames, GameData sortOn, boolean ascending) {
-        switch (sortOn) {
-            case NAME:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getName().compareToIgnoreCase(g2.getName()));
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getName().compareToIgnoreCase(g1.getName()));
-                }
-            case RATING:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> Double.compare(g1.getRating(), g2.getRating()));
-                } else {
-                    return filterGames.sorted((g1, g2) -> Double.compare(g2.getRating(), g1.getRating()));
-                }
-            case DIFFICULTY:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> Double.compare(g1.getDifficulty(), g2.getDifficulty()));
-                } else {
-                    return filterGames.sorted((g1, g2) -> Double.compare(g2.getDifficulty(), g1.getDifficulty()));
-                }
-            case RANK:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getRank() - g2.getRank());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getRank() - g1.getRank());
-                }
-            case MIN_PLAYERS:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getMinPlayers() - g2.getMinPlayers());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getMinPlayers() - g1.getMinPlayers());
-                }
-            case MAX_PLAYERS:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getMaxPlayers() - g2.getMaxPlayers());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getMaxPlayers() - g1.getMaxPlayers());
-                }
-            case MIN_TIME:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getMinPlayTime() - g2.getMinPlayTime());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getMinPlayTime() - g1.getMinPlayTime());
-                }
-            case MAX_TIME:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getMaxPlayTime() - g2.getMaxPlayTime());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getMaxPlayTime() - g1.getMaxPlayTime());
-                }
-            case YEAR:
-                if (ascending) {
-                    return filterGames.sorted((g1, g2) -> g1.getYearPublished() - g2.getYearPublished());
-                } else {
-                    return filterGames.sorted((g1, g2) -> g2.getYearPublished() - g1.getYearPublished());
-                }
-            default:
-                return filterGames;
-        }
     }
 
     @Override
---
+++ src/main/java/student/Sorting.java
@@ -0,0 +1,82 @@
+package student;
+
+import java.util.stream.Stream;
+
+public final class Sorting {
+
+    /**
+     * Constructor for Sorting
+     */
+    private Sorting() {
+
+    }
+
+    /**
+     * Sorts the stream of board games based on the specified column and order.
+     * @param filterGames the stream of board games to be sorted.
+     * @param sortOn the column to sort the results on.
+     * @param ascending whether the sort is in ascending order.
+     * @return a sorted stream of board games based on the specified column and order.
+     */
+    public static Stream<BoardGame> sortOn(Stream<BoardGame> filterGames,
+                                           GameData sortOn, boolean ascending) {
+        switch (sortOn) {
+            case NAME:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getName().compareToIgnoreCase(g2.getName()));
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getName().compareToIgnoreCase(g1.getName()));
+                }
+            case RATING:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> Double.compare(g1.getRating(), g2.getRating()));
+                } else {
+                    return filterGames.sorted((g1, g2) -> Double.compare(g2.getRating(), g1.getRating()));
+                }
+            case DIFFICULTY:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> Double.compare(g1.getDifficulty(), g2.getDifficulty()));
+                } else {
+                    return filterGames.sorted((g1, g2) -> Double.compare(g2.getDifficulty(), g1.getDifficulty()));
+                }
+            case RANK:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getRank() - g2.getRank());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getRank() - g1.getRank());
+                }
+            case MIN_PLAYERS:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getMinPlayers() - g2.getMinPlayers());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getMinPlayers() - g1.getMinPlayers());
+                }
+            case MAX_PLAYERS:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getMaxPlayers() - g2.getMaxPlayers());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getMaxPlayers() - g1.getMaxPlayers());
+                }
+            case MIN_TIME:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getMinPlayTime() - g2.getMinPlayTime());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getMinPlayTime() - g1.getMinPlayTime());
+                }
+            case MAX_TIME:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getMaxPlayTime() - g2.getMaxPlayTime());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getMaxPlayTime() - g1.getMaxPlayTime());
+                }
+            case YEAR:
+                if (ascending) {
+                    return filterGames.sorted((g1, g2) -> g1.getYearPublished() - g2.getYearPublished());
+                } else {
+                    return filterGames.sorted((g1, g2) -> g2.getYearPublished() - g1.getYearPublished());
+                }
+            default:
+                return filterGames;
+        }
+    }
+}
